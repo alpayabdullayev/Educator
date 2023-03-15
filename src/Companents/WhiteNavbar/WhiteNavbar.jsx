fee243036b3136192e4997c24ae0619a358f79d1
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-03-15 23:41:56 +0400

    overlay

--- src/Companents/WhiteNavbar/WhiteNavbar.jsx
+++ src/Companents/WhiteNavbar/WhiteNavbar.jsx
@@ -1,12 +1,12 @@
-import { React, useEffect } from 'react'
+import { React, useEffect, useRef } from 'react'
 import '../WhiteNavbar/whitenavbar.css'
 // Images
-import Image2 from '../../img/SecondNavbar/logo-dark.png'
+import Image2 from '../Navbar/logo-dark.png'
+import { Link } from 'react-router-dom'
 // Icons
 import { AiOutlineSearch } from 'react-icons/ai'
-
-//router-dom
-import {Link} from 'react-router-dom'
+import {AiOutlineBars} from 'react-icons/ai'
+import {GiCrossedPistols} from 'react-icons/gi'
 
 const Navbar = () => {
     useEffect(() => {
@@ -30,16 +30,95 @@ const Navbar = () => {
         };
     }, []);
 
+    const qaraDivUnvaniRef = useRef()
+    const qaraMenyunuAc = (e) => {
+    const kliklenenElement = e.target
+
+
+  if(kliklenenElement.classList.contains('bars-icon')){
+    qaraDivUnvaniRef.current.classList.add('aktiv')
+  }
+}
+
+const qaraMenyunuBagla = (e) => {
+  const kliklenenElement = e.target
+
+
+  if(kliklenenElement.classList.contains('cross-icon')){
+    qaraDivUnvaniRef.current.classList.remove('aktiv')
+  }
+}
+
     return (
         <>
-             <nav className='navbar navbar-expand-lg bg-body-tertiary fixed-top white-navbar'>
+
+
+
+
+    <div className='overlay ' ref={qaraDivUnvaniRef } onClick={qaraMenyunuBagla}>
+      <GiCrossedPistols className='cross-icon' onClick={qaraMenyunuBagla}  />
+
+      <div className='nav-links nav-link fw-bold text-white overlay__nav'>
+      <ul class="navbar-nav ms-auto">
+                            <li class="nav-item">
+                                <Link class="nav-link" aria-current="page" to="/">Home
+                                </Link>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <Link class="nav-link" aria-current="page" to=".">Courses</Link>
+                                <ul class="dropdown-menu">
+                                    <li><Link class="dropdown-item" to="courses-list-4-columns">Courses  List 4 Columns</Link></li>
+                                    <li><Link class="dropdown-item" to="courses-simple-single">Course Simple Single</Link></li>
+
+                                    <li><Link class="dropdown-item" to="">User Dashboard</Link>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <Link class="nav-link" aria-current="page" to="/">Events</Link>
+                                <ul class="dropdown-menu">
+                                    <li><Link class="dropdown-item" to="events-list">Events List</Link></li>
+                                    <li><Link class="dropdown-item" to="event-single">Event Single</Link></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <Link class="nav-link" aria-current="page" to="/">Pages</Link>
+                                <ul class="dropdown-menu">
+                                    <li><Link class="dropdown-item" to="/">Our Teachers</Link></li>
+                                    <li><Link class="dropdown-item" to="/">About Me</Link></li>
+                                    <li><Link class="dropdown-item" to="faq">FAQ Page</Link></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <Link class="nav-link" aria-current="page" to="/">Blog</Link>
+                                <ul class="dropdown-menu">
+                                    <li><Link class="dropdown-item" to="/">Standard List</Link></li>
+                                    <li><Link class="dropdown-item" to="/">Standard Post</Link></li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <Link class="nav-link" aria-current="page" to="/">Contact</Link>
+                            </li>
+
+                        </ul>
+        
+        
+        
+        
+      </div>
+
+
+    </div>
+
+
+            <nav className='navbar navbar-expand-lg bg-body-tertiary fixed-top white-navbar'>
                 <div className="container-fluid">
                     <Link to="/" className="navbar-brand">
                         <img src={Image2} alt="Dark Logo" className="dark-image" />
                     </Link>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                    <button onClick={qaraMenyunuAc} class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <AiOutlineBars className='bars-icon'/>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                         <ul class="navbar-nav ms-auto">
@@ -48,34 +127,34 @@ const Navbar = () => {
                                 </Link>
                             </li>
                             <li class="nav-item dropdown">
-                                <Link class="nav-link" aria-current="page" to="courses-list-4-columns">Courses</Link>
+                                <Link class="nav-link" aria-current="page" to=".">Courses</Link>
                                 <ul class="dropdown-menu">
                                     <li><Link class="dropdown-item" to="courses-list-4-columns">Courses  List 4 Columns</Link></li>
-                                    <li><Link class="dropdown-item" to="courses-list-4-columns/courses-list-4-columns/2">Course Simple Single</Link></li>
+                                    <li><Link class="dropdown-item" to="courses-simple-single">Course Simple Single</Link></li>
 
-                                    <li><Link class="dropdown-item" to="userdashboard">User Dashboard</Link>
+                                    <li><Link class="dropdown-item" to="">User Dashboard</Link>
                                     </li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
-                                <Link class="nav-link" aria-current="page" to="eventslist">Events</Link>
+                                <Link class="nav-link" aria-current="page" to="/">Events</Link>
                                 <ul class="dropdown-menu">
-                                    <li><Link class="dropdown-item" to="eventslist">Events List</Link></li>
-                                    <li><Link class="dropdown-item" to="eventsingle">Event Single</Link></li>
+                                    <li><Link class="dropdown-item" to="events-list">Events List</Link></li>
+                                    <li><Link class="dropdown-item" to="event-single">Event Single</Link></li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
                                 <Link class="nav-link" aria-current="page" to="/">Pages</Link>
                                 <ul class="dropdown-menu">
                                     <li><Link class="dropdown-item" to="/">Our Teachers</Link></li>
-                                    <li><Link class="dropdown-item" to="aboutme">About Me</Link></li>
+                                    <li><Link class="dropdown-item" to="/">About Me</Link></li>
                                     <li><Link class="dropdown-item" to="faq">FAQ Page</Link></li>
                                 </ul>
                             </li>
                             <li class="nav-item dropdown">
                                 <Link class="nav-link" aria-current="page" to="/">Blog</Link>
                                 <ul class="dropdown-menu">
-                                    <li><Link class="dropdown-item" to="standartlist">Standard List</Link></li>
+                                    <li><Link class="dropdown-item" to="/">Standard List</Link></li>
                                     <li><Link class="dropdown-item" to="/">Standard Post</Link></li>
                                 </ul>
                             </li>
@@ -91,7 +170,6 @@ const Navbar = () => {
                     </div>
                 </div>
             </nav>
-
         </>
     )
 }
